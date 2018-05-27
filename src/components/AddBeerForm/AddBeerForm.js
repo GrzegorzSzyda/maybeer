6ece8153560650c4e0d9b2c9437e35ea257dd6ce
Grzegorz Szyda <kontakt@grzegorzszyda.pl>
2018-05-27 21:12:16 +0200

    Add beer template

--- src/components/AddBeerForm/AddBeerForm.js
+++ src/components/AddBeerForm/AddBeerForm.js
@@ -4,11 +4,42 @@ import styled from 'styled-components';
 
 type AddBeerFormProps = {};
 
-export const AddBeerForm = (props: AddBeerFormProps) => <Container>Dodawanie</Container>;
+export const AddBeerForm = (props: AddBeerFormProps) => (
+    <Container>
+        <PhotoField>Tu bedą zdjęcia</PhotoField>
+        <NameField type="text" placeholder="Nazwa piwa" />
+        <RateField>Ocena</RateField>
+        <DiscriptionField placeholder="Opis" />
+    </Container>
+);
 
 const Container = styled.div`
-    padding: 10px;
+    display: grid;
+    height: calc(100vh - 70px);
+    grid-template-rows: 210px auto auto 1fr;
+    grid-gap: 1em;
+    padding: 1em;
+`;
+
+const PhotoField = styled.div`
+    background: rgba(255, 255, 255, 0.1);
+    font-size: 1.3em;
+`;
+
+const NameField = styled.input`
+    font: inherit;
     background: #fff;
-    border-radius: 4px;
-    box-shadow: 3px 10px 20px rgba(0, 0, 0, 0.1);
+    font-size: 1.3em;
+    padding: 1em;
+`;
+const RateField = styled.div`
+    background: #fff;
+    font-size: 1.3em;
+    padding: 1em;
+`;
+const DiscriptionField = styled.textarea`
+    font: inherit;
+    background: #fff;
+    font-size: 1.3em;
+    padding: 1em;
 `;
